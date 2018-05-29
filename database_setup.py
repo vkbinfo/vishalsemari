8162ed29f930406e16596bbc3f27c59212d1f140
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2018-05-30 00:17:41 +0530

    Changed setting for image reference

--- database_setup.py
+++ database_setup.py
@@ -1,37 +1,37 @@
-#sqlalchemy configuration start same for every app
+# sqlalchemy configuration start same for every app
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
-Base=declarative_base()
+Base = declarative_base()
 
 
-
-#configuration complete the above code is bascially required for every sqlalchemy app
+# configuration complete the above code is bascially required for every sqlalchemy app
 
 class News(Base):
-    __tablename__="news"
+    __tablename__ = "news"
     id = Column(Integer, primary_key=True)
     link = Column(String(500))
-    post=Column(String(100), nullable=False)
-
+    post = Column(String(100), nullable=False)
 
 
 class Tender(Base):
-    __tablename__="tender"
+    __tablename__ = "tender"
     id = Column(Integer, primary_key=True)
     link = Column(String(500))
     post = Column(String(100), nullable=False)
+
 
 class Headlines(Base):
-    __tablename__="headlines"
+    __tablename__ = "headlines"
     id = Column(Integer, primary_key=True)
     link = Column(String(500))
     post = Column(String(100), nullable=False)
 
-#initialize database and table
 
-engine=create_engine('sqlite:///semari.db')
+# initialize database and table
+
+engine = create_engine('sqlite:///semari.db')
 
 Base.metadata.create_all(engine)
